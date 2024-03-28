35d9296b083749795278fc7b3adf927a41ed70f5
eripratama1 <eripratama90@gmail.com>
2024-03-28 17:54:38 +0800

    commit 280324

--- app/Http/Controllers/SocialiteLoginController.php
+++ app/Http/Controllers/SocialiteLoginController.php
@@ -11,22 +11,30 @@ class SocialiteLoginController extends Controller
 {
     public function redirectToProvider()
     {
+        // Memulai login sosial dengan provider Google
+        // Ini akan mengalihkan pengguna ke halaman login Google untuk otorisasi.
         return Socialite::driver('google')->redirect();
     }
 
     public function handleProviderCallback()
     {
+        // Mendapatkan informasi pengguna dari Google setelah otorisasi berhasil
         $socialiteUser = Socialite::driver('google')->user();
 
+        // Mencari atau membuat pengguna lokal berdasarkan alamat email Google
         $user = User::firstOrCreate(
             ['email' => $socialiteUser->getEmail()],
             ['name' => $socialiteUser->getName()]
         );
+
+        // (Opsional) Membuat catatan terkait di tabel SocialiteLogin
+        // Ini berguna untuk melacak penyedia login sosial yang berbeda yang digunakan oleh pengguna.
         SocialiteLogin::firstOrCreate(
             ['user_id' => $user->id, 'provider' => 'google'],
             ['provider_id' => $socialiteUser->getId()]
         );
 
+        // Mengotentikasi pengguna di aplikasi lokal
         auth()->login($user);
         return to_route('home');
     }
